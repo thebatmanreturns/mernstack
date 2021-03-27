707b104db0580a5897dbf7b2db924a18006fbff6
karan256540@gmail.com <karan256540@gmail.com>
2021-03-27 22:05:09 +0530

    This is commit 2 after completing the course section2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import {BrowserRouter as Router,Route} from 'react-router-dom'; 
+import {Container,Row,Col} from 'react-bootstrap';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 const App = () => {
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
+    <Router>
+    <Header />
+      <Container>
+      <main className='py-3'>
+      <Route path='/' component={HomeScreen} exact />
+      <Route path='/product/:id' component={ProductScreen} />
+      </main>
+      </Container>
+    <Footer />
+    </Router>
   );
 }
 
